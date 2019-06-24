274983cfa8ba37089326b2a43633e595746aa846
Frank Vumbaca <fvumbaca@outlook.com>
2019-06-24 19:12:49 -0400

    Added emacs configuration to strapped

---
+++ config/emacs_init.el
@@ -0,0 +1,43 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages
+   (quote
+    (evil-paredit clojure-mode paredit rainbow-delimiters ido-completing-read+ cider flycheck slime emojify ace-window exec-path-from-shell alchemist elixir-mode powerline projectile spacemacs-theme evil-leader evil org-plus-contrib use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(let ((gc-cons-threshold most-positive-fixnum))
+
+  ;; Set repositories
+  (require 'package)
+  (setq-default
+   load-prefer-newer t
+   package-enable-at-startup nil)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (package-initialize)
+
+  ;; Install dependencies
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package t))
+  (setq-default
+   use-package-always-defer t
+   use-package-always-ensure t)
+
+  ;; Use latest Org
+  (use-package org :ensure org-plus-contrib)
+
+  ;; Tangle configuration
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+  (garbage-collect))
